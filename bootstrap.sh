f0b74da3142719132e6c59679ba7d592ea9b4af1
pdunnigan <patrick.dunnigan@cloudsidekick.com>
2014-03-13 16:55:03 -0400

    more mods to bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,10 +11,10 @@ mkdir -p /var/chef/tmp
 chmod 755 -R /var/chef
 curl https://www.opscode.com/chef/install.sh | /bin/bash
 
-echo '"run_list": [ "recipe[petclinic::default]" ]}' > /tmp/solo.json
+echo '{"run_list": [ "recipe[petclinic::default]" ]}' > /tmp/solo.json
 
 chef-solo -j /tmp/solo.json -c /var/chef/solo.rb
 
 # for some reason, the install doesn't complete until it is run again
 # the second time around there should be no error
-chef-solo -j /var/chef/solo.json -c /var/chef/solo.rb
+chef-solo -j /tmp/solo.json -c /var/chef/solo.rb
