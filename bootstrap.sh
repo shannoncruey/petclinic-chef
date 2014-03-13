3a7698f873feccafcaaf6b3b30941f527533df56
pdunnigan <patrick.dunnigan@cloudsidekick.com>
2014-03-13 16:35:00 -0400

    altered bootstrap process

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,13 +4,17 @@
 # mostly to be used on a dev machine run via vagrant
 # currently ubuntu flavored only
 
-apt-get update
-apt-get -y --quiet install git curl
+#apt-get update
+#apt-get -y --quiet install git curl
+apt-get -y --quiet install curl
 git clone https://github.com/cloudsidekick/petclinic-chef.git /var/chef
 mkdir -p /var/chef/tmp
 chmod 755 -R /var/chef
 curl https://www.opscode.com/chef/install.sh | /bin/bash
-chef-solo -j /var/chef/solo.json -c /var/chef/solo.rb
+
+echo '"run_list": [ "recipe[petclinic::default]" ]}' > /tmp/solo.json
+
+chef-solo -j /tmp/solo.json -c /var/chef/solo.rb
 
 # for some reason, the install doesn't complete until it is run again
 # the second time around there should be no error
