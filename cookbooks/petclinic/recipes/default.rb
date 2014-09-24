6d044991b7dcac012d012328603009c3f5840a7e
pdunnigan <patrick.dunnigan@cloudsidekick.com>
2014-09-24 09:50:19 -0400

    Changed default installation directory

--- cookbooks/petclinic/recipes/default.rb
+++ cookbooks/petclinic/recipes/default.rb
@@ -9,7 +9,7 @@
 include_recipe "application"
 
 application 'petclinic' do
-  path         '/opt/petclinic'
+  path         '/usr/local/petclinic'
   owner        node['tomcat']['user']
   group        node['tomcat']['group']
   repository   node['petclinic']['war']['url']
